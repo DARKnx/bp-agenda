150b582f550ddeaa354da56dc1af36735178617a
Dutra PC <joao.vitornl@gmail.com>
2024-01-13 13:43:26 -0300

    :construction: Create layout and avatar component

---
+++ src/components/layout/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar class="px-3 pe-10">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-title class="ms-auto text-h6 font-weight-bold" :style="{ color: $vuetify.theme.themes.light.colors.primary }">
+        RESERVA
+        <span class="ms-auto text-h6 font-weight-black"
+          :style="{ color: $vuetify.theme.themes.light.colors.secondary }">BP</span>
+      </v-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <div class="px-3" style="display: flex; height: 100%; flex-direction: column; justify-content: space-between;">
+        <div>
+          <v-sheet class="pa-6"
+            style="flex-direction: row; display: flex; align-items:  center; justify-content: space-around;">
+            <avatar :name="store.user.name" :size="50" />
+            <div>
+              <v-text class="text-subtitle font-weight-bold">
+                {{ formatName(store.user.name, true) }}
+              </v-text>
+            </div>
+          </v-sheet>
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item v-for="[icon, text, route] in defaultLinks" :key="icon" :to="route" link
+              :class="{ 'v-list-item--active': route === currentRoute }">
+              <v-list-item-content class="d-flex px-6">
+                <v-list-item-icon class="pr-5">
+                  <v-icon>{{ icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title class="">{{ text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </div>
+
+        <v-list class="pb-4">
+          <v-divider class="py-2"></v-divider>
+          <v-list-item v-for="[icon, text, route] in settingsLinks" :key="icon" :to="route" link
+            :class="{ 'v-list-item--active': route === currentRoute }">
+            <v-list-item-content class="d-flex px-6">
+              <v-list-item-icon class="pr-5">
+                <v-icon>{{ icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="">{{ text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </div>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid class="custom-container fill-height">
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
+
+<script setup>
+    import { ref, watchEffect } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    import { useUserStore } from '../../stores/user.ts';
+    import formatName from '../../utils/formatName.ts';
+    import Avatar from '../avatar/index.vue';
+    import './index.css'
+
+    const store = useUserStore();
+    const drawer = ref(null);
+    const route = useRoute();
+    const currentRoute = ref(route.path);
+
+    const defaultLinks = [
+      ['mdi mdi-calendar-account', 'Agenda', '/dashboard'],
+      ['mdi mdi-account-box', 'Minha conta', '/minha-conta'],
+      ['mdi mdi-history', 'Histórico', '/historico'],
+    ];
+
+    const settingsLinks = [
+      ['mdi mdi-cog', 'Configurações', '/configuracoes'],
+      ['mdi mdi-logout', 'Sair', '/logout'],
+    ];
+
+    watchEffect(() => {
+      currentRoute.value = route.path;
+    });
+</script>
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,89 +1,19 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar class="px-3 pe-10">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-title class="ms-auto">BP AGENDA</v-title>
-    </v-app-bar>
+    <Layout>
 
-    <v-main>
-
-    </v-main>
-
-    <v-navigation-drawer v-model="drawer">
-      <div class="px-3" style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; ">
-        <div>
-          <v-sheet class="pa-6" style="flex-direction: row; display: flex; align-items:  center; justify-content: space-around;">
-            <avatar :name="store.user.name" :size="50" />
-            <div>
-              <div>{{ store.user.name }}</div>
-            </div>
-          </v-sheet>
-          <v-divider></v-divider>
-
-
-          <v-list>
-            <v-list-item
-              v-for="[icon, text, route] in links"
-              :key="icon"
-              :to="route"
-              link
-              :class="{ 'v-list-item--active': isActiveRoute(route) }"
-            >
-              <v-list-item-content class="d-flex px-6" >
-                <v-list-item-icon class="pr-5">
-                  <v-icon>{{ icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title class="">{{ text }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </div>
-
-        <v-list class="pb-4">
-          <v-divider class="py-2"></v-divider>
-          <v-list-item
-            v-for="[icon, text, route] in links2"
-            :key="icon"
-            :to="route"
-            link
-            :class="{ 'v-list-item--active': isActiveRoute(route) }"
-          >
-            <v-list-item-content class="d-flex px-6">
-              <v-list-item-icon class="pr-5">
-                <v-icon>{{ icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title class="">{{ text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </div>
-    </v-navigation-drawer>
-  </v-app>
-</template>
-
-<script setup>
-import { ref, computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { useUserStore } from '../../stores/user.ts';
-import Avatar from '../../components/layout/avatar.vue';
-
-const store = useUserStore();
-
-const links = [
-  ['mdi mdi-calendar-account', 'Agenda', '/agenda'],
-  ['mdi mdi-history', 'Histórico', '/historico'],
-  ['mdi mdi-account-box', 'Minha conta', '/minha-conta'],
-];
-
-const links2 = [
-  ['mdi mdi-cog', 'Configurações', '/configuracoes'],
-  ['mdi mdi-logout', 'Sair', '/logout'],
-  ];
-
-const drawer = ref(null);
-const route = useRoute();
-
-const isActiveRoute = (targetRoute) => {
-  return computed(() => route.path === targetRoute).value;
-};
-</script>
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col>
+              ola mundo
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </Layout>
+  </template>
+  
+  <script setup>
+  import Layout from '../../components/layout/index.vue'
+  </script>
+  
