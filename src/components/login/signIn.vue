6d4545f38326cbc47641002e6af66c4ae5c8df19
Dutra PC <joao.vitornl@gmail.com>
2024-01-14 01:53:36 -0300

    :truck: chore: Rename routes in login pages

--- src/components/login/signIn.vue
+++ src/components/login/signIn.vue
@@ -34,7 +34,7 @@
       const user = await signIn({ email: email.value, password: password.value });
       if (user?.error) return toast.error(user.error);
       toast.success('Autenticação bem-sucedida');
-      router.push('/dashboard');
+      router.push('/dashboard/schedule');
     };
 </script>
 
