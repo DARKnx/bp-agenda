aef29441537256203a32fbf27be5107bea22a657
Dutra PC <joao.vitornl@gmail.com>
2024-01-15 04:08:47 -0300

    :construction: Create getBrokerSchedule function

--- src/actions/schedule.ts
+++ src/actions/schedule.ts
@@ -24,3 +24,14 @@ export const getBrokers = async (): Promise<any> => {
         return { error: (err instanceof AxiosError) ? err.response?.data.msg : 'Erro desconhecido.' };
     }
 }
+
+export const getBrokersSchedule = async (): Promise<any> => {
+    try {
+        const response = await api.post('user/broker-schedule');
+        const data = response.data;
+        return data;
+
+    } catch (err) {
+        return { error: (err instanceof AxiosError) ? err.response?.data.msg : 'Erro desconhecido.' };
+    }
+}
